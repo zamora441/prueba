d55beb9e7c688cc4254e011ee92c293770630c98
zamora441 <101614191+zamora441@users.noreply.github.com>
2023-02-03 01:57:39 -0600

    prueba html5-qrcode

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,58 @@
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import QRCode from 'qrcode'
-import QRScanner from 'cordova-plugin-qrscanner'
+import QrScanner from 'qr-scanner'
+import {Html5Qrcode} from 'html5-qrcode'
 
 function App() {
   const [count, setCount] = useState(0);
+  const [data, setData]= useState('Contenido codigo')
+  const [data2, setData2]= useState('Contenido codigo2')
   const canvasRef = useRef(null)
-  const videoRef = useRef(null)
+  const videoRef = useRef()
   const divRef = useRef(null)
-
   const qrImage = QRCode.toCanvas(canvasRef.current,'holaaaaa', { errorCorrectionLevel: 'H' }, (error,) => {
     if (error) {
       console.log(error);
     }
   })
 
-  const done = (error, status) => {
-    if (error) {
-    console.log(error._message);
+  const scanner = (decodedText, decodedResult) => {
+    setData(decodedResult)
+    setData2(decodedText)
+  } 
+  
+  const config = {
+    fps: 10,
+    qrbox: {
+      width: 250,
+      height: 250
     }
   }
   
-  const displayContents = (error, content) => {
-    if(error){
-      console.log(error._message);
-    } else {
-      console.log(content);
-    }
-  }
+  let html5QrCode;
 
+  useEffect(() => {
+    html5QrCode = new Html5Qrcode("reader")
+  }, [])
 
+  
+  
   return (
-    <div className="App" ref={divRef}>
+    <div className="App" >
+      <p>{ data}</p>
+      <p>{ data2}</p>
+      <div id="reader" ></div>
       <button onClick={() => {
         console.log('click');
-        QRScanner.scan(displayContents)
-        QRScanner.show(status => {
-          console.log(status);
-        })
+        /*   const scanner = new QrScanner(videoRef.current, result => console.log(result), { returnDetailedScanResult: true }) */
+        
+        html5QrCode.start({ facingMode: "environment" }, config, scanner)
       }}>escanear</button>
 
       <canvas ref={canvasRef}></canvas>
-
-      <video ref={videoRef} ></video>
+      <video ref={videoRef} disablePictureInPicture playsInline ></video>
     </div>
   )
 }
